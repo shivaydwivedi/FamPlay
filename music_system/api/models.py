9969558cb2a2eb410024d92ffc0e3e616c0855e2
shivaydwivedi <polymath1510@gmail.com>
2024-12-28 04:19:29 +0530

    Step 5,6  done

--- music_system/api/models.py
+++ music_system/api/models.py
@@ -17,12 +17,8 @@ def generate_unique_code():
 # Our models here.
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True) #Code to enter the Room
-    host = models.CharField(max_length=50, unique=True)#Host to enter their name
-    guest_can_pause = models.BooleanField(null=False, default=False)#allows our guests to pause the song
-    votes_to_skip = models.IntegerField(null=False, default=1)#members can vote to skip a song
-    created_at = models.DateTimeField(auto_now_add=True) #this automatically adds a Date and Time when the Room is created
-
-
-    
-
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
